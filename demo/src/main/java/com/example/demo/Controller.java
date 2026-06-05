168e7e8defd08013dea5a23e6234f4679c35f1a7
Crx08 <cesare.sonaglia@gmail.com>
2026-06-05 11:32:23 +0200

    prove

--- demo/src/main/java/com/example/demo/Controller.java
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable; // <-- Aggiunta
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody; // <-- Aggiunta
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,15 @@ public class Controller {
     public String sayHello() {
         return "Ciao dal tuo primo progetto Spring Boot!";
     }
-}
 
+    @GetMapping("/saluta/{nome}")
+    public String salutaPersona(@PathVariable String nome) {
+        return "Ciao, " + nome + "!";
+    }
+
+
+    @PostMapping("/invia")
+    public String riceviDati(@RequestBody String datiRicevuti) {
+        return "Ho ricevuto i tuoi dati: " + datiRicevuti;
+    }
+}
