8dd12147a05fab26b876e843039a3c0c357b6388
Cesare Sonaglia <sonaglia@micrapg.lan>
2026-06-08 10:32:48 +0200

    prova2

---
+++ demo/demo/src/main/java/com/example/demo/Entity/Autore.java
@@ -0,0 +1,37 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity //Tabella del database
+@Table(name = "autori") //La tabella si chiama "autori"
+public class Autore {
+
+    @Id // Chiave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    @Column(name = "id_autore")
+    private Long idAutore;
+
+    private String nome;
+    private String cognome;
+
+    @Column(name = "data_nascita")
+    private LocalDate dataNascita; // Il tipo DATE di SQL diventa LocalDate in Java
+
+    @Column(name = "data_morte")
+    private LocalDate dataMorte;
+
+    public Autore() {}
+
+    // Getter e Setter
+    public Long getIdAutore() { return idAutore; }
+    public void setIdAutore(Long idAutore) { this.idAutore = idAutore; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCognome() { return cognome; }
+    public void setCognome(String cognome) { this.cognome = cognome; }
+    public LocalDate getDataNascita() { return dataNascita; }
+    public void setDataNascita(LocalDate dataNascita) { this.dataNascita = dataNascita; }
+    public LocalDate getDataMorte() { return dataMorte; }
+    public void setDataMorte(LocalDate dataMorte) { this.dataMorte = dataMorte; }
+}
--- demo/demo/src/main/java/com/example/demo/Entity/Main.java
+++ demo/demo/src/main/java/com/example/demo/Entity/Main.java
@@ -1,13 +1,14 @@
-package com.example.demo;
+package com.example.demo.Entity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(Main.class, args);
 	}
 
 }
