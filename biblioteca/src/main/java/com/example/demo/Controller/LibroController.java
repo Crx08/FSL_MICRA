770cf8dc1aa02dc75bf130fd323ee9bf50849f2e
Crx08 <cesare.sonaglia@gmail.com>
2026-06-08 10:49:28 +0200

    progetto portato in maven

---
+++ biblioteca/src/main/java/com/example/demo/Controller/LibroController.java
@@ -0,0 +1,20 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Entity.Libro;
+import com.example.demo.Repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/libri")
+public class LibroController {
+
+    @Autowired
+    private LibroRepository libroRepository;
+
+    @GetMapping
+    public List<Libro> ottieniTuttiILibri() {
+        return libroRepository.findAll();
+    }
+}
---
+++ biblioteca/src/main/java/com/example/demo/Repository/PrestitoRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.Prestito;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrestitoRepository extends JpaRepository<Prestito, Long> {
+}
